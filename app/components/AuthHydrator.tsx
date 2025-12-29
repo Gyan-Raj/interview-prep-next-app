36677faa39da800fb97af496d9dd6d3b4debbc24
Gyan-Raj7758 <bhardwaj7739@gmail.com>
2025-12-29 18:20:59 +0530

    corrected type errors

--- app/components/AuthHydrator.tsx
+++ app/components/AuthHydrator.tsx
@@ -3,8 +3,9 @@
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { setUser } from "@/app/store/slices/authSlice";
+import { AuthUser } from "../types";
 
-export default function AuthHydrator({ user }) {
+export default function AuthHydrator({ user }: { user: AuthUser }) {
   const dispatch = useDispatch();
 
   useEffect(() => {
