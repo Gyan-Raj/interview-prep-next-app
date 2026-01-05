94f3da57ce92bb9c90e76c0820316704aacb5da4
Gyan-Raj7758 <bhardwaj7739@gmail.com>
2026-01-05 13:15:37 +0530

    admin module: optional check operator

--- app/api/admin/roles/route.ts
+++ app/api/admin/roles/route.ts
@@ -150,7 +150,7 @@ export async function GET() {
   const roles = await prisma.role.findMany({
     select: { id: true, name: true },
   });
-  let validRoles = roles.filter((r) => r.id !== authUser.activeRole.id);
+  let validRoles = roles.filter((r) => r.id !== authUser?.activeRole?.id);
 
   return NextResponse.json(validRoles);
 }
